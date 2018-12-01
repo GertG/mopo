ac299b63b5b96f9978a2093e6ad53b60b665f625
Gert <gert.gielen1@gmail.com>
2018-12-01 15:30:23 +0100

    More specific import.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {Router} from '@angular/router';
 import { HostListener } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 import { PatientService } from '../patient.service';
 import { Patient, Gender } from '../model/patient';
